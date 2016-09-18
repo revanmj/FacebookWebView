fd0595cc40f8e3070766728550432662408faad2
Michał Jakubowski <revanmj@gmail.com>
2016-09-18 18:07:38 +0200

    1.0.3
    - Updated SDK to Nougat
    - Added support for Marshmallow permission model
    - Updated AdvancedWebView library
    - Updated Gradle plugin version

--- app/src/main/java/pl/revanmj/facebookwebview/MainActivity.java
+++ app/src/main/java/pl/revanmj/facebookwebview/MainActivity.java
@@ -1,15 +1,20 @@
 package pl.revanmj.facebookwebview;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
+import android.widget.Toast;
 
 import im.delight.android.webview.AdvancedWebView;
 
@@ -30,12 +35,11 @@ public class MainActivity extends ActionBarActivity {
 
         webview = (AdvancedWebView) findViewById(R.id.webView);
         webview.getSettings().setAppCacheEnabled(true);
-        webview.setGeolocationEnabled(true);
         webview.addPermittedHostname("www.facebook.com");
         webview.addPermittedHostname("touch.facebook.com");
         webview.addPermittedHostname("m.facebook.com");
         webview.addPermittedHostname("h.facebook.com");
-
+        webview.addPermittedHostname("facebook.com");
         webview.setListener(this, new AdvancedWebView.Listener() {
 
             @Override
@@ -57,6 +61,17 @@ public class MainActivity extends ActionBarActivity {
             }
 
             @Override
+            public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
+                if (AdvancedWebView.handleDownload(MainActivity.this, url, suggestedFilename)) {
+                    Log.d(MainActivity.class.getSimpleName(), "File download finished.");
+                }
+                else {
+                    Log.d(MainActivity.class.getSimpleName(), "File download failed.");
+                    // TODO show some notice to the user
+                }
+            }
+
+            @Override
             public void onExternalPageRequest(String url) {
                 //AlertDialog ad = new AlertDialog.Builder(MainActivity.this).create();
                 //ad.setMessage(url);
@@ -72,14 +87,42 @@ public class MainActivity extends ActionBarActivity {
                 startActivity(i);
             }
 
-            @Override
-            public void onDownloadRequested(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
-                // some file is available for download
-            }
-
         });
 
         webview.loadUrl("http://www.facebook.com/");
+        checkForPermission();
+    }
+
+    private void checkForPermission() {
+        int permission = ContextCompat.checkSelfPermission(this, "android.permission.ACCESS_FINE_LOCATION");
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, "android.permission.ACCESS_FINE_LOCATION")) {
+            Toast.makeText(this, "Location permission denied, disabling geolocation support...", Toast.LENGTH_LONG).show();
+            webview.setGeolocationEnabled(false);
+            return;
+        }
+        else if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{"android.permission.ACCESS_FINE_LOCATION"}, 1);
+            return;
+        }
+
+        webview.setGeolocationEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    webview.setGeolocationEnabled(true);
+                } else {
+                    Toast.makeText(this, "Location permission denied, disabling geolocation support...", Toast.LENGTH_SHORT).show();
+                    webview.setGeolocationEnabled(false);
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     @Override
@@ -120,7 +163,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
         webview.onActivityResult(requestCode, resultCode, intent);
-        // your normal onActivityResult(...) code here
     }
 
     @Override
