307fdda3206b49fbaf65e34704fd25773319ab45
Michał Jakubowski <revanmj@gmail.com>
2019-11-17 09:53:12 +0100

    Updating libs, sdk level and a little code cleanup

--- app/src/main/java/pl/revanmj/facebookwebview/MainActivity.kt
+++ app/src/main/java/pl/revanmj/facebookwebview/MainActivity.kt
@@ -24,7 +24,24 @@ import java.util.ArrayList
 import im.delight.android.webview.AdvancedWebView
 
 class MainActivity : AppCompatActivity() {
-    private var mWebView: AdvancedWebView? = null
+    companion object {
+        private val LOG_TAG = MainActivity::class.java.simpleName
+        
+        private const val FB_URL = "https://www.facebook.com/"
+        private const val FB_SHARE_URL = "https://www.facebook.com/sharer.php?u="
+        private const val PERMISSION_REQUEST_CODE = 1
+        private val PERMITTED_HOSTNAMES = object : ArrayList<String>() {
+            init {
+                add("www.facebook.com")
+                add("touch.facebook.com")
+                add("m.facebook.com")
+                add("h.facebook.com")
+                add("facebook.com")
+            }
+        }
+    }
+
+    private val mWebView: AdvancedWebView = findViewById(R.id.webView)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,10 +53,9 @@ class MainActivity : AppCompatActivity() {
             CookieSyncManager.createInstance(applicationContext)
         CookieManager.getInstance().setAcceptCookie(true)
 
-        mWebView = findViewById(R.id.webView)
-        mWebView!!.settings.setAppCacheEnabled(true)
-        mWebView!!.addPermittedHostnames(PERMITTED_HOSTNAMES)
-        mWebView!!.setListener(this, object : AdvancedWebView.Listener {
+        mWebView.settings.setAppCacheEnabled(true)
+        mWebView.addPermittedHostnames(PERMITTED_HOSTNAMES)
+        mWebView.setListener(this, object : AdvancedWebView.Listener {
             override fun onPageFinished(url: String) {
                 if (Build.VERSION.SDK_INT > 20)
                     CookieManager.getInstance().flush()
@@ -85,7 +101,7 @@ class MainActivity : AppCompatActivity() {
                 val shareText = intent.getStringExtra(Intent.EXTRA_TEXT)
                 if (Patterns.WEB_URL.matcher(shareText).matches()) {
                     try {
-                        mWebView!!.loadUrl(FB_SHARE_URL + URLEncoder.encode(shareText, "UTF-8"))
+                        mWebView.loadUrl(FB_SHARE_URL + URLEncoder.encode(shareText, "UTF-8"))
                         super.onNewIntent(intent)
                         return
                     } catch (e: UnsupportedEncodingException) { }
@@ -96,7 +112,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        mWebView!!.loadUrl(FB_URL)
+        mWebView.loadUrl(FB_URL)
     }
 
     private fun checkForPermission() {
@@ -104,23 +120,23 @@ class MainActivity : AppCompatActivity() {
 
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
             Toast.makeText(this, R.string.location_permission_denied, Toast.LENGTH_LONG).show()
-            mWebView!!.setGeolocationEnabled(false)
+            mWebView.setGeolocationEnabled(false)
             return
         } else if (permission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_CODE)
             return
         }
-        mWebView!!.setGeolocationEnabled(true)
+        mWebView.setGeolocationEnabled(true)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
             PERMISSION_REQUEST_CODE -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                mWebView!!.setGeolocationEnabled(true)
+                mWebView.setGeolocationEnabled(true)
             } else {
                 Toast.makeText(this, R.string.location_permission_denied, Toast.LENGTH_SHORT).show()
-                mWebView!!.setGeolocationEnabled(false)
+                mWebView.setGeolocationEnabled(false)
             }
             else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
@@ -141,29 +157,12 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
         super.onActivityResult(requestCode, resultCode, intent)
         // Making back key going to the previous site
-        mWebView!!.onActivityResult(requestCode, resultCode, intent)
+        mWebView.onActivityResult(requestCode, resultCode, intent)
     }
 
     override fun onBackPressed() {
-        if (mWebView!!.onBackPressed()) {
+        if (mWebView.onBackPressed()) {
             super.onBackPressed()
-        }
-    }
-
-    companion object {
-        private val LOG_TAG = MainActivity::class.java.simpleName
-        private const val FB_URL = "https://www.facebook.com/"
-        private const val FB_SHARE_URL = "https://www.facebook.com/sharer.php?u="
-        private const val PERMISSION_REQUEST_CODE = 1
-
-        private val PERMITTED_HOSTNAMES = object : ArrayList<String>() {
-            init {
-                add("www.facebook.com")
-                add("touch.facebook.com")
-                add("m.facebook.com")
-                add("h.facebook.com")
-                add("facebook.com")
-            }
         }
     }
 }
